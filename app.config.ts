f758487f8040f7749e1e8b0c4b112b440ff3444e
matiasrozenblum <rozenblummatias@gmail.com>
2026-03-24 23:57:50 -0300

    v1.0.1

--- app.config.ts
+++ app.config.ts
@@ -103,7 +103,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name: getAppName(),
         slug: "la-guia-del-streaming",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: getAppIcon(),
         userInterfaceStyle: "dark",
